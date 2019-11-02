a92700ccac6270043dee35be28db0269e3daf221
Joey Wunderlich <jwunderl@users.noreply.github.com>
2019-11-01 23:54:52 -0700

    method stubs

--- main.ts
+++ main.ts
@@ -1,1 +1,56 @@
+type PromiseResult<T> = T | PromiseLike<T>;
 
+interface PromiseLike<T> {
+    then<TResult1 = T, TResult2 = never>(
+        onfulfilled?: (value: T) => PromiseResult<TResult1>,
+        onrejected?: (reason: any) => PromiseResult<TResult2>
+    ): PromiseLike<TResult1 | TResult2>;
+}
+
+class Promise<T> implements PromiseLike<T> {
+    public constructor(
+        executor: (
+            resolve: (value?: T | PromiseLike<T>) => void,
+            reject: (reason?: any) => void
+        ) => void
+    ) {
+        // not yet implemented
+    }
+
+    public then<TResult1 = T, TResult2 = never>(
+        onfulfilled?: (value: T) => PromiseResult<TResult1>,
+        onrejected?: (reason: any) => PromiseResult<TResult2>
+    ): Promise<TResult1 | TResult2> {
+        return undefined; // not yet implemented
+    }
+
+    public catch<TResult = never>(
+        onrejected?: (reason: any) => PromiseResult<TResult>
+    ): Promise<T | TResult> {
+        return undefined; // not yet implemented
+    }
+
+    public finally(onfinally?: () => void): Promise<T> {
+        return undefined; // not yet implemented
+    }
+
+    public static all<T>(values: (PromiseResult<T>)[]): Promise<T[]> {
+        return undefined; // not yet implemented
+    }
+
+    public static allSettled<T>(values: (PromiseResult<T>)[]): Promise<T[]> {
+        return undefined; // not yet implemented
+    }
+
+    public static race<T>(values: T[]): Promise<T> {
+        return undefined; // not yet implemented
+    }
+
+    public static reject<T = never>(reason?: any): Promise<T> {
+        return undefined; // not yet implemented
+    }
+
+    public static resolve<T>(value: PromiseResult<T>): Promise<T> {
+        return undefined; // not yet implemented
+    }
+}
